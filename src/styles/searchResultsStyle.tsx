1790caf95da5b3327bfcbf8683551af717900975
WonWoo-Shin <sww0481@gmail.com>
2025-05-23 03:37:23 +0900

    검색 결과 media qurery 적용

--- src/styles/searchResultsStyle.tsx
+++ src/styles/searchResultsStyle.tsx
@@ -28,6 +28,18 @@ export const ResultsList = styled.ul`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   gap: 60px 8px;
+  @media (max-width: 1400px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  @media (max-width: 1100px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 500px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const ItemImage = styled.img`
