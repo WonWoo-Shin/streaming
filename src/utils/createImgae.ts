050d825883d783bcc0a87c7d7000ff51b1e37e19
WonWoo-Shin <sww0481@gmail.com>
2025-07-24 18:59:54 +0900

    이미지 없을시 대체 이미지 추가

--- src/utils/createImgae.ts
+++ src/utils/createImgae.ts
@@ -1,3 +1,5 @@
 export const createImage = (size: string, path: string) => {
-  return `https://image.tmdb.org/t/p/${size}${path}`;
+  return path
+    ? `https://image.tmdb.org/t/p/${size}${path}`
+    : "/img/emptyImage.png";
 };
